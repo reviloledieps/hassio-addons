0ed8b26dbe3d32a9159eab3267cad6a8ace620bc
reviloledieps <speidel.o@gmx.net>
2022-08-21 17:17:16 +0200

    Chmod again

--- tvheadend/rootfs/etc/cont-init.d/customizations.sh
+++ tvheadend/rootfs/etc/cont-init.d/customizations.sh
@@ -20,5 +20,5 @@ if bashio::config.has_value 'init_commands'; then
     while read -r cmd; do
         eval "${cmd}" \
             || bashio::exit.nok "Failed executing init command: ${cmd}"
-    done <<< "$(bashio::config 'init_commands')" 
+    done <<< "$(bashio::config 'init_commands')"
 fi
