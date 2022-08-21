d5599db8fa591d76c0e82c0fe1c64304e82231cc
reviloledieps <speidel.o@gmx.net>
2022-08-21 17:54:21 +0200

    Header missing

--- tvheadend/rootfs/etc/cont-init.d/tvheadend.sh
+++ tvheadend/rootfs/etc/cont-init.d/tvheadend.sh
@@ -1,3 +1,9 @@
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+# Home Assistant Community Add-on: TVHeadend
+# Executes user customizations on startup
+# ==============================================================================
+
 # Ensure directory exists
 if ! bashio::fs.directory_exists '/config/tvheadend/'; then
     bashio::log.info "Creating default configuration directory at /config/tvheadend/"
